5fe1b2a8a6b2eaa5bbfd545de4e903ad94e6a7e2
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-07 20:22:44 -0400

    Redesign and implement

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
 //! Show all environment variables, or particular ones.
 //!
-//! With no arguments, this prints all environment variables. If arguments are
-//! passed, then they are glob patterns and only environment variables whose
-//! names match at least one of the patterns are printed. In either case,
-//! variables are printed in lexicographic order and never more than once.
-//!
-//! No options are recognized. Environment variable names may begin with `-` on
-//! some systems, and even such arguments are taken to be patterns. Environment
-//! variables are case-sensitive on many systems and at least case-aware on
-//! most, but globbing is performed case-insensitively. Because this globbing
-//! is for convenience rather than related to filenames, `.` and `/` are not
-//! treated specially.
+//! Note that this is not currently customized for Windows, where it would be
+//! more useful to sort and deduplicate case-insensitively and, most of all, to
+//! always show the name with whatever specific casing it is represented with
+//! in the environment.
+
+fn print_one_var(name: &str, value: &str) {
+    println!("{name}={value}");
+}
+
+fn print_all_vars() {
+    let mut pairs: Vec<_> = std::env::vars().collect();
+    pairs.sort_by(|(lhs_name, _), (rhs_name, _)| lhs_name.cmp(rhs_name));
+    for (name, value) in &pairs {
+        print_one_var(name, value);
+    }
+}
+
+fn print_some_vars(mut names: Vec<String>) {
+    names.sort();
+    names.dedup();
+    for name in &names {
+        if let Ok(value) = std::env::var(name) {
+            print_one_var(name, &value);
+        }
+    }
+}
 
 fn main() {
-
+    let names: Vec<_> = std::env::args().skip(1).collect();
+    if names.is_empty() {
+        print_all_vars();
+    } else {
+        print_some_vars(names);
+    }
 }
